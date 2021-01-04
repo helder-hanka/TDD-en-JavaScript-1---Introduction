f9813e70f78442668b031260044af6881f0e0741
helder-hanka <hfernandes238@gmail.com>
2021-01-04 23:34:00 +0100

    Separate tests from the code to be tested

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,6 +1,3 @@
-// capitalizeFirst.js
-const assert = require('assert');
-
 // WRITE THE ACTUAL FUNCTION HERE
 
 function capitalizeFirstLetters (input) {
@@ -8,16 +5,4 @@ function capitalizeFirstLetters (input) {
     ? input.split(' ').map( i => i[0].toUpperCase() + i.slice(1)).join(' ')
     : '';
 }
-
-// Check that capitalizeFirst is a function
-assert.strictEqual(typeof capitalizeFirstLetters, 'function');
-
-assert.strictEqual(capitalizeFirstLetters.length, 1);
-
-assert.strictEqual(capitalizeFirstLetters('i am learning TDD'), 'I Am Learning TDD');
-
-assert.strictEqual(capitalizeFirstLetters('z'), 'Z');
-
-assert.strictEqual(capitalizeFirstLetters(''), '');
-
-console.log(capitalizeFirstLetters('i am learning TDD'))
+module.exports = capitalizeFirstLetters;
